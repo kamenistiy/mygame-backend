57ff0b6ce973b8b97a3f113f78d853a5ebc0d7fa
Сергей Орлов <orlov98wf@gmail.com>
2026-05-24 22:21:52 +0300

    eee

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def ping():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
